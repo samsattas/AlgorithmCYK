b0ba64c13bcbf35ab605f0e5ca93d64cc976b9e8
JuanOssa1 <juan.ossa2@correo.icesi.edu.co>
2021-05-22 20:34:46 -0500

    Correccion error representante

--- AlgorithmCYK/Model/CYK.cs
+++ AlgorithmCYK/Model/CYK.cs
@@ -8,33 +8,43 @@ namespace AlgorithmCYK.Model
 {
     class CYK
     {
-        public List<List<string>> matrix;
+        public string w;
+        public List<List<List<string>>> matrix;
         public List<string> data; 
-        //public String dataEntered;
-
-        public CYK() {
-            matrix = new List<List<string>>();
+        //w=abc
+        public CYK(string w) {
+            matrix = new List<List<List<string>>>();
             data = new List<string>();
+            init(w);
+        }
+        public void init(string w) {
+            for (int i =0; i < w.Length; i++) {
+                List<List<string>> initList = new List<List<string>>();
+                matrix.Add(initList);
+            }
         }
         public void addData(string gramathic) {
             data.Add(gramathic);
         }
-        public string checkGramathic(string toCheck) {
-            string representative = "";
-            List<string> lineArray = new List<string>();
-            string[] auxArray = toCheck.Split(',');
-            for (int i = 0; i < auxArray.Length; i++) {
-                lineArray.Add(auxArray[i]);
-            }
-            for (int i = 0; i < lineArray.Count; i++) {
-                for (int j = 1; j < auxArray.Length; j++) {
-                    if (lineArray.ElementAt(i).ElementAt(j).Equals(toCheck)) {
-                        representative += lineArray.ElementAt(i).ElementAt(0) + ","; //REVISAR QUE NO QUEDE ASI POR EJEMPLO AB,AX, SI NO AB,AX
+        public List<string> checkGramathic(string toCheck)
+        {
+            List<string> arrayReturn = new List<string>();
+            for (int i = 0; i < data.Count; i++) {
+                string[] auxArray = data.ElementAt(i).Split(',');
+                for (int j = 1; j < auxArray.Length; j++){
+                    if (data.ElementAt(i).ElementAt(j).Equals(toCheck)){
+                        arrayReturn.Add(data.ElementAt(i).ElementAt(0) + "");
                     }
                 }
             }
-            return representative;
+            return arrayReturn;
         }
-
+        public Boolean checkAcceptation() {
+            Boolean value = false;
+            if (matrix.ElementAt(matrix.Count-1).ElementAt(0).Equals("S")) {
+                value = true;
+            }
+            return value;
+        }
     }
 }
